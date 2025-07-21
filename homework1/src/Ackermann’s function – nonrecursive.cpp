fd9a449f484bcf67a836bb3bb217f6569d76926d
張家誠 <155621741+Adsgfjhk@users.noreply.github.com>
2025-07-21 19:35:13 +0800

    Update Ackermann’s function – nonrecursive.cpp

--- homework1/src/Ackermann’s function – nonrecursive.cpp
+++ homework1/src/Ackermann’s function – nonrecursive.cpp
@@ -8,7 +8,7 @@ int main()
 	int x = 2;
 	cin >> m >> n;
 	start = clock();
-	switch (m)//看 m 的不同來對應相對的公式
+	switch (m)
 	{
 	case 0:
 		n++;
@@ -23,7 +23,7 @@ int main()
 		n = pow(2, n + 3) - 3;
 		break;
 	case 4:
-		for (int i = 0; i < n + 2; i++)//m = 4 為 n+3 個 2 的 2 次方
+		for (int i = 0; i < n + 2; i++)
 		{
 			ans = pow(2, x);
 			x = ans;
