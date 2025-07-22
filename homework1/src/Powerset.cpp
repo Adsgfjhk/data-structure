dbc9db4cfb20ad7c41778e57ac6e84a7928f4d39
張家誠 <155621741+Adsgfjhk@users.noreply.github.com>
2025-07-22 11:23:01 +0800

    Update Powerset.cpp

--- homework1/src/Powerset.cpp
+++ homework1/src/Powerset.cpp
@@ -6,9 +6,9 @@ void powerset(string s, string a, int n)
 		cout << "( " << a << " )" << endl;
 	else
 	{
-		powerset(s, a, n + 1);//先執行遞迴一次，讓空集合可以輸出
-		a += s[n];//將使用者自訂的字串加入新的字串中
-		powerset(s, a, n + 1);//將新增的字串遞迴會有不同排列方式
+		powerset(s, a, n + 1);
+		a += s[n];
+		powerset(s, a, n + 1);
 	}
 }
 int main()
@@ -17,7 +17,7 @@ int main()
 	string a;//使用第二個字串來儲存要輸出的資料
 	cin >> x;
 	clock_t start = clock();
-	powerset(x, a, 0);//n 為記錄該遞迴程式是否到底了
+	powerset(x, a, 0);
 	clock_t stop = clock();
 	cout << "花費時間：" << double(stop - start) / CLOCKS_PER_SEC << endl;
 }
