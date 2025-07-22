73fb32b5c5b10df212029654a8d0cf382ac4fd76
張家誠 <155621741+Adsgfjhk@users.noreply.github.com>
2025-07-22 09:50:24 +0800

    Update Ackermann’s function – recursive.cpp

--- homework1/src/Ackermann’s function – recursive.cpp
+++ homework1/src/Ackermann’s function – recursive.cpp
@@ -1,26 +1,22 @@
 #include <iostream>
-#include <ctime>
 using namespace std;
 long long Ackermann(long long m, long long n)
 {
 	if (m == 0)
-		return n + 1; 
+		return n + 1;
 	else if (m > 0 && n == 0)
-		return Ackermann(m - 1, 1); 
+		return Ackermann(m - 1, 1);
 	else
-		return Ackermann(m - 1, Ackermann(m, n - 1)); 
+		return Ackermann(m - 1, Ackermann(m, n - 1));
 }
 int main()
 {
 	long long m, n;
 	long long a;
-	double start, end; 
-	double cost;
-	start = clock(); 
+	clock_t start = clock();
 	cin >> m >> n;
 	a = Ackermann(m, n);
 	cout << a << endl;
-	end = clock(); 
-	cost = end - start; 
-	cout << "時間差：" << cost / 1000 << "秒" << endl;
+	clock_t stop = clock();
+	cout << "花費時間：" << double(stop - start) / CLOCKS_PER_SEC << "秒" << endl;
 }
